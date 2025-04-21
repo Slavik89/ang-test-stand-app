54f8ced3afdff8a4c47b028effeeace2c09feeec
Yaroslav Kostiv <slovjans89@yahoo.com>
2025-04-21 18:13:39 +0300

    Template-driven form was created

--- src/app/components/simple-form/simple-form.component.ts
+++ src/app/components/simple-form/simple-form.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-simple-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './simple-form.component.html',
   styleUrl: './simple-form.component.scss'
 })
 export class SimpleFormComponent {
+
+  name: string = '';
+  email: string = '';
+
+  submitForm(formValue: {name: string, email: string}) {
+    // console.log('Form submitted', {name: this.name, email: this.email});
+    console.log('Form submitted', formValue);
+  }
 
 }
